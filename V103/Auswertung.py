902a899f7bc5ff2daeccc5c721a1eeb3513bc7ab
Carl Arne Thomann <arnethomann@me.com>
2014-10-14 18:01:26 +0200

    v1.0

--- V103/Auswertung.py
+++ V103/Auswertung.py
@@ -1,20 +1,25 @@
+
+import numpy as np 										#### Header
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import math
 from uncertainties import ufloat
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 16
-import numpy as np
 
-d = np.genfromtxt('Durchmesser.txt').T
+
+d = np.genfromtxt('Durchmesser.txt').T 					#### Einladen der Daten
 s1 = np.genfromtxt('Seite1.txt').T
 s2 = np.genfromtxt('Seite2.txt').T
+xrecht,drecht = np.genfromtxt('RechteckigerStab1.txt').T
+xrund,drund = np.genfromtxt('RunderStab1.txt').T
+x2,d2rechts, d2links = np.genfromtxt('Zweiseitig.txt').T
 
-durch = ufloat(np.mean(d),np.std(d))
+durch = ufloat(np.mean(d),np.std(d)) 					#### Berechnung der Werte
 seite1 = ufloat(np.mean(s1),np.std(s1))
 seite2 = ufloat(np.mean(s2),np.std(s2))
 
 
-print("Der Durchmesser betraegt:",durch,"mm")
+print("Der Durchmesser betraegt:",durch,"mm") 			#### Ausgabe
 print("Die eine Seite betraegt:",seite1,"mm")
 print("Der Durchmesser betraegt:",seite2,"mm")
---
+++ V103/Graphen.py
@@ -0,0 +1,33 @@
+import numpy as np 								#### Header
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+from uncertainties import ufloat
+plt.rcParams['figure.figsize'] = (10, 8)
+plt.rcParams['font.size'] = 12
+
+
+d = np.genfromtxt('Durchmesser.txt').T 			#### Einladen der Daten
+s1 = np.genfromtxt('Seite1.txt').T
+s2 = np.genfromtxt('Seite2.txt').T
+xrecht,drecht = np.genfromtxt('RechteckigerStab1.txt').T
+xrund,drund = np.genfromtxt('RunderStab1.txt').T
+x2,d2rechts, d2links = np.genfromtxt('Zweiseitig.txt').T
+
+def schoenePlots(y):							#### Funktion, um x-Array zu drehen
+	return 550-y
+x3 = schoenePlots(x2)
+x3 = x3[::-1]
+d2links = d2links[::-1]
+
+#print(x2)
+#print(x3)
+
+plt.plot(xrecht,drecht,label='Rechteckiger Stab')#### Die Plots
+plt.plot(xrund,drund,label="Runder Stab")
+plt.plot(x2,d2rechts,label="Zweiseitige Einspannung, rechts")
+plt.plot(x3,d2links,label="Zweiseitige Einspannung, links")
+plt.legend(loc="best")
+plt.xlabel("x in mm")
+plt.ylabel("Auslenkung in mm")
+plt.show()
