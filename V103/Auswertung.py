baecf071c6cbb7019a8298f73260ddf873eaf1c8
Carl Arne Thomann <arnethomann@me.com>
2014-10-14 17:26:40 +0200

    Messwerte und Python-Skript

--- V103/Auswertung.py
+++ V103/Auswertung.py
@@ -1,11 +1,20 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import math
+from uncertainties import ufloat
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 16
 import numpy as np
 
-a, b ,c = np.genfromtxt('Zweiseitig.txt').T
+d = np.genfromtxt('Durchmesser.txt').T
+s1 = np.genfromtxt('Seite1.txt').T
+s2 = np.genfromtxt('Seite2.txt').T
 
-print(a)
-print(b)
-print(c)
+durch = ufloat(np.mean(d),np.std(d))
+seite1 = ufloat(np.mean(s1),np.std(s1))
+seite2 = ufloat(np.mean(s2),np.std(s2))
+
+
+print("Der Durchmesser betraegt:",durch,"mm")
+print("Die eine Seite betraegt:",seite1,"mm")
+print("Der Durchmesser betraegt:",seite2,"mm")
