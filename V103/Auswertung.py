29b85729bdbb1fe7ca30e6c95d45331ea0f7b963
Carl Arne Thomann <arnethomann@me.com>
2014-10-15 11:10:39 +0200

    v1.3

--- V103/Auswertung.py
+++ V103/Auswertung.py
@@ -20,15 +20,17 @@ seite1 = ufloat(np.mean(s1),np.std(s1))
 seite2 = ufloat(np.mean(s2),np.std(s2))
 
 Irund = 1/4*(durch/2)**(4)*np.pi 						#### Berechnung der Flächenträgheitsmomente
-Irecht = 1/3*(seite1)**(3)*seite2
+Irecht = 1/12*seite2**3*seite1
 
 print("Der Durchmesser betraegt:",durch,"mm") 			#### Ausgabe
 print("Der Radius betraegt:",durch/2,"mm")
 print("Die x-Seite betraegt:",seite1,"mm")
 print("Der y-Seite betraegt:",seite2,"mm")
+print("")
 print("Fuer die Flaechentraegheitsmomente gelten:")
 print("Rund:",Irund)
 print("Recht:",Irecht)
 
-
-
+plt.plot(492*xrund**2-xrund**3/3,drund)
+plt.plot(492*xrecht**2-xrecht**3/3,drecht)
+plt.show()
