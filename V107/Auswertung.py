7ec3e6cbbed3e4592ca7f614e330221371fdeba4
HelenaChiara <h.nawrath@yahoo.de>
2014-10-22 11:37:00 +0200

    Stand Mittwoch Vormittag
    Neue Auswertung
    Literaturwerte.pdf hinzu

--- V107/Auswertung.py
+++ V107/Auswertung.py
@@ -16,12 +16,12 @@ klein = np.genfromtxt('Messung1_Raumtemperatur_klein.txt').T
 ta25,ta30,ta35,ta40,ta45,ta50,ta55,ta60,ta65,ta70 = np.genfromtxt('Messung2_Temperaturabhaengigkeit.txt')
 
 
-mG=4.61			# in g											#### Konstanten
-mK=4.44			# in g
-rG=1.058/2 		# in cm
-rK=1.0565/2 	# in cm
-rhoW = 0.998203 # Dichte des Wassers bei 20 °C in g/cm^3
-apparatK = 76.4
+mG=0.00461			# in kg											#### Konstanten
+mK=0.00444			# in kg
+rG=0.01058/2 		# in m
+rK=0.010565/2 	# in m
+rhoW = 997.54 # Dichte des Wassers bei 23 C in kg/m^3
+apparatK = 7.64*10**(-8) #Pa m³/kg
 
 g = ufloat(np.mean(gross),stats.sem(gross)) 					#### Berechnung der Werte
 k = ufloat(np.mean(klein),stats.sem(klein))
@@ -38,7 +38,7 @@ t70 = ufloat(np.mean(ta70),stats.sem(ta70))
 fallzeit = np.array([t25,t30,t35,t40,t45,t50,t55,t60,t65,t70])
 
 rhoG = mG/((4/3)*np.pi*(rG)**3)
-rhoK = mK/((4/3)*np.pi*(rK)**3)
+rhoK = mK/((4/3)*np.pi*((rK)**3))
 
 etaK= apparatK*(rhoK-rhoW)*k
 apparatG=etaK/((rhoG-rhoW)*g)
@@ -58,19 +58,20 @@ print("")
 #print("Fuer 65c:",t65)	
 #print("Fuer 70c:",t70)
 #print("")
-#print("Zeit fuer die grosse Kugel:",g,"sec")
-#print("Zeit fue die kleine Kugel:",k,"sec")
-#print("")
-print("Die Dichte der grossen Kugel:", rhoG, "g/cm^3")
-print("Die Dichte der kleinen Kugel:", rhoK, "g/cm^3")
+print("Zeit fuer die grosse Kugel:",g,"sec")
+print("Zeit fue die kleine Kugel:",k,"sec")
+print("")
+print("Die Dichte der grossen Kugel:", rhoG, "kg/m^3")
+print("Die Dichte der kleinen Kugel:", rhoK, "kg/m^3")
 print("")
 print("Eta, bestimmt mit der kleinen Kugel, ist:",etaK)
 print("Mit diesem Eta ist die Apparaturkonstante:", apparatG)
 print("")
 
 
-T = np.arange(25,75,5)											#### Regressionen
-T_func = np.linspace(0,0.05)
+T = np.arange(25,75,5)+273.15
+print(T)										#### Regressionen
+T_func = T#np.linspace(0,0.05)
 
 def f(x, m, b):
     return m*x+b
@@ -82,13 +83,15 @@ def linregress(x, y):
     sigma_y = np.sqrt(np.sum((y - m * x - b)**2) / (N - 2))
     m_error = sigma_y * np.sqrt(N / Delta)
     b_error = sigma_y * np.sqrt(np.sum(x**2) / Delta)
-    print ("Die Regressionssteigung ist:", m,"+/-", m_error)
+    print ("Die Regressionssteigung/B ist:", m,"+/-", m_error)
     print ("Der Regressions-Y-Achsenabschnitt ist:", b,"+/-", b_error)
+    print("A ist:",np.exp(b),"+/-",np.sqrt(np.exp(2*b)*b_error**2))
+    return(m,m_error,b,b_error) 
 
 																#### Plots
 etaG =unp.log(apparatG*(rhoG-rhoW)*fallzeit)
 linregress(1/T,noms(etaG))
-#etaG= np.log(noms(apparatG))+np.log(rhoG-rhoW)+np.log(noms(fallzeit))
+etaG_2= (noms(apparatG))*(rhoG-rhoW)*(noms(fallzeit))
 #etaG_neu= unp.log(apparatG)+unp.log(rhoG-rhoW)+unp.log(fallzeit)
 
 
@@ -101,5 +104,5 @@ plt.errorbar(1/T, noms(etaG) ,yerr=stds(etaG),fmt="bx", label="Werte")
 plt.legend(loc="best")
 plt.tight_layout
 plt.show()
-
+print(etaG_2)
 
