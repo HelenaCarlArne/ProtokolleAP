6e759bbbbd753ae62b83a2dda4d9aef7ead3ef07
Carl Arne Thomann <arnethomann@me.com>
2014-11-06 17:32:00 +0100

    V204: Graphen-Quellcode

--- V204/content/Auswertung.py
+++ V204/content/Auswertung.py
@@ -19,4 +19,4 @@ ID_M1, T1_M1, T2_M1, T3_M1, T4_M1, T5_M1, T6_M1, T7_M1, T8_M1 = np.genfromtxt(".
 ID_M2, T1_M2, T5_M2, T2_M2, T6_M2, Junk_M2 = np.genfromtxt("../Originalwerte/Messung80").T 
 ID_M3, T1_M3, T2_M3, T3_M3, T4_M3, T5_M3, T6_M3, T7_M3, T8_M3, Junk_M3= np.genfromtxt("../Originalwerte/Messung200",delimiter = ',').T
 
-#peakdetect()
+peakdetect()
