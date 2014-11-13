2b62c6cd46fb67f5bb53269f1a06af862835ea5b
HelenaChiara <h.nawrath@yahoo.de>
2014-11-13 15:53:44 +0100

    aktualisierung

--- V206/Fehlerrechnung_Gueteziffer.py
+++ V206/Fehlerrechnung_Gueteziffer.py
@@ -26,7 +26,19 @@ nu_real_2=13209*0.031/(N_delta)
 nu_real_3=13209*0.027/(N_delta)
 nu_real_4=13209*0.017/(N_delta)
 
+T11=23.0	
+T21=21.2
+T12=33.7	
+T22=11.5
+T13=44.2	
+T23=1.8
+T14=49.8	
+T24=-0.7
 
+nu_ideal1=T11/(T11-T21)
+nu_ideal2=T12/(T12-T22)
+nu_ideal3=T13/(T13-T23)
+nu_ideal4=T14/(T14-T24)
 
 print('Mittelwertleistung',N_MW,'+/-',N_Fehler)
 print('N für die Berechnung zur den Zeiten t_i',N_delta)
@@ -36,4 +48,12 @@ print(nu_real_1,'+-',delta_nu_real_1)
 print(nu_real_2,'+-',delta_nu_real_2)
 print(nu_real_3,'+-',delta_nu_real_3)
 print(nu_real_4,'+-',delta_nu_real_4)
+print('')
+print('nu ideal')
+print('nu_ideal1',nu_ideal1)
+print('nu_ideal2',nu_ideal2)
+print('nu_ideal3',nu_ideal3)
+print('nu_ideal4',nu_ideal4)
+print('nu ideal')
+
 
