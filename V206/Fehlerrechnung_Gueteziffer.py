d47f81f594888fd26e7536beff25f24eed7bd004
HelenaChiara <h.nawrath@yahoo.de>
2014-11-16 17:07:12 +0100

    Abgabe aktualisiert Rechnung von N und L hinzugefügt

--- V206/Fehlerrechnung_Gueteziffer.py
+++ V206/Fehlerrechnung_Gueteziffer.py
@@ -19,23 +19,23 @@ delta_N_t=np.sqrt(1/3*((175+420+205)/4))
 
 
 delta_nu_real_1=np.sqrt(((13209*0.002)/N_t)**2+((13209*0.0223*delta_N_t)/(N_t**2))**2)
-delta_nu_real_2=np.sqrt(((13209*0.005)/N_t)**2+((13209*0.031*delta_N_t)/(N_t**2))**2)
-delta_nu_real_3=np.sqrt(((13209*0.009)/N_t)**2+((13209*0.027*delta_N_t)/(N_t**2))**2)
-delta_nu_real_4=np.sqrt(((13209*0.013)/N_t)**2+((13209*0.017*delta_N_t)/(N_t**2))**2)
+delta_nu_real_2=np.sqrt(((13209*0.004)/N_t)**2+((13209*0.032*delta_N_t)/(N_t**2))**2)
+delta_nu_real_3=np.sqrt(((13209*0.007)/N_t)**2+((13209*0.027*delta_N_t)/(N_t**2))**2)
+delta_nu_real_4=np.sqrt(((13209*0.009)/N_t)**2+((13209*0.017*delta_N_t)/(N_t**2))**2)
 
 nu_real_1=13209*0.0223/(N_t)
-nu_real_2=13209*0.031/(N_t)
+nu_real_2=13209*0.032/(N_t)
 nu_real_3=13209*0.027/(N_t)
 nu_real_4=13209*0.017/(N_t)
 
-T11=23.0	
-T21=21.2
-T12=33.7	
-T22=11.5
-T13=44.2	
-T23=1.8
-T14=49.8	
-T24=-0.7
+T11=296.15
+T21=294.35
+T12=306.85	
+T22=284.65
+T13=317.35	
+T23=274.95
+T14=322.95	
+T24=272.45
 
 nu_ideal1=T11/(T11-T21)
 nu_ideal2=T12/(T12-T22)
@@ -47,10 +47,10 @@ print(N)
 print('N für die Berechnung zur den Zeiten t_i',N_t, '+-',delta_N_t)
 print('')
 print('nu real')
-print(delta_nu_real_1,'+-',delta_nu_real_1)
-print(delta_nu_real_2,'+-',delta_nu_real_2)
-print(delta_nu_real_3,'+-',delta_nu_real_3)
-print(delta_nu_real_4,'+-',delta_nu_real_4)
+print(nu_real_1,'+-',delta_nu_real_1)
+print(nu_real_2,'+-',delta_nu_real_2)
+print(nu_real_3,'+-',delta_nu_real_3)
+print(nu_real_4,'+-',delta_nu_real_4)
 print('')
 print('nu ideal')
 print('nu_ideal1',nu_ideal1)
