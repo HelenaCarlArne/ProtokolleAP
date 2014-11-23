024e0eb20716f0561a144d9c6720e25fb8e701e1
Carl Arne Thomann <arnethomann@me.com>
2014-11-23 14:57:45 +0100

    V204: Berichtigung abgeschlossen. V201: Auswertung iW fertig.
    Es fehlen:
    Quellenverzeichnis
    Referenzen auf Gleichungen in Theorie
    Letzter Schliff.

--- V201/Werte/WaermKap_Proben.py
+++ V201/Werte/WaermKap_Proben.py
@@ -1,3 +1,20 @@
+"""
+
+ Information
+
+
+Dieses Skript gibt die Wärmekapazitäten und die Molwärmen der Proben aus.
+
+In Block "Werteeingabe gemäß Symbole" werden die in der Anleitung gegebenen Werte eingetragen.
+In Block "Berechnung" werden Funktionen definiert, die die Wärmekapazitäten ausgibt [warmth()] oder 
+die Molwärmen bei konstantem Volumen  [otherwarmth()] ausgibt.
+Die Parameter sind so benannt, wie man sie schreiben würde (siehe Funktionendefinition)
+In dem Ausgabe-Block sind die Funktionen enthalten, die Parameter stammen vom Experiment/Anleitung.
+
+Die vierte, abgetrennte Zahl bei Blei ist der Mittelwert; 
+der zusätzliche Term bei der Molwärmen bei konstantem Volumen ist die Differenz der Molwärmen.
+"""
+
 #
 # Header
 #
@@ -71,7 +88,7 @@ print(warmth(574,385.55,23.5,82.9,25.9)*Mpb)
 print(warmth(537,385.55,21.8,91.0,25.2)*Mpb)
 print(warmth(583,385.55,22.3,75.7,24.5)*Mpb)
 print("")
-# siehe Bem. oben
+# analog zur Bem. bei Blei oben
 a1=ufloat(np.mean([574,537,583]),sem([574,537,583]))
 a2=385.55
 a3=ufloat(np.mean([23.5,21.8,22.3]),sem([23.5,21.8,22.3]))
@@ -100,28 +117,28 @@ print(pb3)
 print("")
 pbm= ufloat(np.mean([pb1,pb2,pb3]),sem([pb1,pb2,pb3]))
 print(pbm)
-print("")
 print(otherwarmth(29*10**(-6),42*10**9,207.2,11.35,25.9+273.15))
 print("")
 print("Graphit")
 cm = warmth(544,107.23,22.5,83.3,25.6)*Mc-otherwarmth(8*10**(-6), 33*10**9,12,2.25,25.6+273.15)
 print(cm)
-print("")
 print(otherwarmth(8*10**(-6), 33*10**9,12,2.25,25.6+273.15))
 print("")
 print("Kupfer")
 cum = warmth(562,139.77,22.1,82.3,26.4)*Mcu-otherwarmth(16.8*10**(-6),136*10**9,63.5,8.96,26.4+273.15)
 print(cum)
-print("")
 print(otherwarmth(16.8*10**(-6),136*10**9,63.5,8.96,26.4+273.15))
 print("")
-
-
+print("")
+print("")
+print("")
+print("Die Abweichungen vom DP-Gesetz")
 R = ufloat(8.3144621,0.0000075)
-
+print("Blei")
 print((pbm-3*R)/(3*R))
+print("")
+print("Graphit")
 print((cm-3*R)/(3*R))
+print("")
+print("Kupfer")
 print((cum-3*R)/(3*R))
-
-
-
