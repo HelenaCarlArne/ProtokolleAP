276af5eda856d8161eded64bdea6d6c59f1a43eb
Carl Arne Thomann <arnethomann@me.com>
2015-05-14 20:13:47 +0200

    V601: Erster Teil der Auswertung vollständig 14.05.2015
    Es wurde der erste Teil der Auswertung fertiggestellt.
    Hierzu wurden die Bilder der Energieverteilungen und deren
    Ableitungen geplottet.
    Die Nullstelle des linearen Fits ist fehlerbehaftet.
    
    Es fehlen noch die Auswertungsteile II und III
    
    Anmerkung: Bitte die 0.05mm-Werte im Blick behalten

--- V601 - Franck-Hertz/content/auswertung.py
+++ V601 - Franck-Hertz/content/auswertung.py
@@ -11,6 +11,8 @@
 import numpy as np 
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from uncertainties import unumpy as unp
+from uncertainties import ufloat
 #
 ##	Kalibrierung
 ###
@@ -22,7 +24,7 @@ def linear(x,a,b):
 	return a*x+b
 
 name = np.array(["Ekalt","Ewarm","Ion","FH"])
-""" AUSGABE_ORDNER
+
 for v in name:
 	parameter, coeffmatrix =curve_fit(linear, eval("x_length_%s"%(v)),eval("x_quant_%s"%(v)))
 	error=np.sqrt(np.diag(coeffmatrix))
@@ -39,7 +41,7 @@ for v in name:
 
 print('*********************************************')
 print('*********************************************')
-"""
+
 
 
 #
@@ -50,11 +52,14 @@ print('*********************************************')
 Ediff_y=np.array([])
 u_vert_kalt,i_vert_kalt	=np.genfromtxt("../Werte/Vert_kalt.txt").T
 
-plt.plot(u_vert_kalt,i_vert_kalt,"x")
+plt.plot(u_vert_kalt,i_vert_kalt,"x",label="Messdaten")
 plt.xlabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
 plt.ylabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
-plt.savefig("../Bilder/Vert_kalt.ps")
+plt.legend()
+plt.savefig("../Bilder/Vert_kalt.pdf")
 plt.show()
+
+
 for i in range(0,np.size(u_vert_kalt)-1):
 	#print((i_vert_kalt[i]-i_vert_kalt[i+1])/(u_vert_kalt[i+1]-u_vert_kalt[i]))
 	Ediff_y=np.append(Ediff_y,(i_vert_kalt[i]-i_vert_kalt[i+1])/(u_vert_kalt[i+1]-u_vert_kalt[i]))
@@ -62,13 +67,27 @@ for i in range(0,np.size(u_vert_kalt)-1):
 for i in range(0,np.size(u_vert_kalt)-1):
 	u_vert_kalt[i]=(u_vert_kalt[i+1]+u_vert_kalt[i])/2
 
-plt.plot(np.delete(u_vert_kalt,-1),Ediff_y,"+")
-#print(np.size(np.delete(u_vert_kalt,-1)))
-#print(np.size(Ediff_y))
+plt.plot(np.delete(u_vert_kalt,-1),Ediff_y,"+",label="Errechnete Steigung")
+parameter,coeffmatrix=curve_fit(linear,u_vert_kalt[-9:-1],Ediff_y[-9:-1])
+
+plt.plot(u_vert_kalt[-13:-1],linear(u_vert_kalt[-13:-1],*parameter),label="Fit der fallenden Flanke")
+plt.xlim(0,27)
+plt.ylim(0,1.6)
 plt.xlabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
 plt.ylabel(r"$\mathrm{\ddot{A}nderung\,der\,Diagramml\ddot{a}nge\,in}\,cm$")
-plt.savefig("../Bilder/Vert_kalt_diff.ps")
+plt.legend(loc="best")
+plt.savefig("../Bilder/Vert_kalt_diff.pdf")
 plt.show()
+
+errors=np.sqrt(np.diag(coeffmatrix))
+parameter=unp.uarray(parameter,errors)
+print("Die Nullstelle der Tangente ist:")
+print(-parameter[1]/parameter[0])
+print(ufloat(0.4036,0.0044)*-parameter[1]/parameter[0]+ufloat(-0.1246,0.0579))
+
+#ufloat{0.4036,0.0044}
+#ufloat{-0.1246,0.0579}
+
 "" "AUSGABE_ORDNER"
 
 
@@ -79,31 +98,41 @@ plt.show()
 Ediff_y=np.array([])
 u_vert_warm,i_vert_warm	=np.genfromtxt("../Werte/Vert_warm.txt").T
 
-plt.plot(u_vert_warm,i_vert_warm,"x")
+plt.plot(u_vert_warm,i_vert_warm,"x",label="Messdaten")
 plt.xlabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
 plt.ylabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
-plt.savefig("../Bilder/Vert_warm.ps")
+plt.legend()
+plt.savefig("../Bilder/Vert_warm.pdf")
 plt.show()
+
+
 for i in range(0,np.size(u_vert_warm)-1):
 	#print((i_vert_warm[i]-i_vert_warm[i+1])/(u_vert_warm[i+1]-u_vert_warm[i]))
 	Ediff_y=np.append(Ediff_y,(i_vert_warm[i]-i_vert_warm[i+1])/(u_vert_warm[i+1]-u_vert_warm[i]))
 
-
 for i in range(0,np.size(u_vert_warm)-1):
 	u_vert_warm[i]=(u_vert_warm[i+1]+u_vert_warm[i])/2
 
-
-#print(np.size(np.delete(u_vert_warm,-1)))
-#print(np.size(Ediff_y))
-plt.plot(np.delete(u_vert_warm,-1),Ediff_y,"+")
+plt.plot(np.delete(u_vert_warm,-1),Ediff_y,"+",label="Errechnete Steigung")
 plt.xlabel(r"$\mathrm{Diagramml\ddot{a}nge\,in}\,cm$")
 plt.ylabel(r"$\mathrm{\ddot{A}nderung\,der\,Diagramml\ddot{a}nge\,in}\,cm$")
-plt.savefig("../Bilder/Vert_warm_diff.ps")
+
+parameter,coeffmatrix=curve_fit(linear,np.delete(u_vert_warm,[23,24,25,26,27,28,29,30]),np.delete(Ediff_y,[23,24,25,26,27,28,29]))
+plt.plot(np.delete(u_vert_warm,[24,25,26,27,28,29,30]),linear(np.delete(u_vert_warm,[24,25,26,27,28,29,30]),*parameter),label="Fit der fallenden Flanke")
+plt.xlim(0,25)
+plt.ylim(0,2)
+plt.legend()
+plt.savefig("../Bilder/Vert_warm_diff.pdf")
 plt.show()
 
+errors=np.sqrt(np.diag(coeffmatrix))
+parameter=unp.uarray(parameter,errors)
+print("Die Nullstelle der Tangente ist:")
+print(-parameter[1]/parameter[0])
+print(ufloat(0.4047,0.0040)*-parameter[1]/parameter[0]+ufloat(0.0154,0.0594))
 
-#print(u_vert_warm,i_vert_warm)
-
+#ufloat{0.4047,0.0040}
+#ufloat{0.0154,0.0594}
 
 #
 ##	Energieverteilung, warm
