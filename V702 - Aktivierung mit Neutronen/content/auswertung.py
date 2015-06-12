2a85f082f77ec6b23972f864a4350dec242c62ae
Carl Arne Thomann <arnethomann@me.com>
2015-06-12 22:54:07 +0200

    V702: Auswertung und Diskussion fertig - bitte überprüfen
    Problematik: Auswertung der kurzlebigen Zustände noch nicht schlüssig.

--- V702 - Aktivierung mit Neutronen/content/auswertung.py
+++ V702 - Aktivierung mit Neutronen/content/auswertung.py
@@ -27,7 +27,7 @@ N_ind=unp.uarray(N_ind-nom(n0)*250,N_ind_err-std(n0)*250)
 print("INDIUM:")
 print("Berichtigung:")
 print("")
-#print(N_ind)
+print(N_ind)
 N_ind=unp.log(N_ind)
 
 def f(t, a, b):
@@ -38,13 +38,10 @@ errors = np.sqrt(np.diag(covariance))
 print("Parameter:")
 a=ufloat(params[0],errors[0])
 print("a={}".format(a))
-print("Da={}".format((a-0.0002128)))##################################################
-print("da={}".format(((a-0.0002128)/0.0002128)))##################################################
 b=ufloat(params[1],errors[1])
 print("b={}".format(b))
 print("")
 print('T ={}'.format(np.log(2)/-a))
-print('DT ={}'.format((np.log(2)/-a)-3257))
 print('N0 ={}'.format(unp.exp(b)/(1-unp.exp(a*250))))
 print("")
 X = np.linspace(0, 4000)
@@ -59,9 +56,7 @@ plt.legend(loc="best")
 plt.tight_layout()
 #plt.yscale("log")
 plt.savefig("../Bilder/indium.pdf")
-
 plt.close()
-
 #
 ##	RHODIUM
 ###
@@ -72,18 +67,23 @@ N_rho_err=np.sqrt(N_rho)
 n_rho=unp.uarray(N_rho-nom(n0)*20,N_rho_err-std(n0)*20)
 print("RHODIUM:")
 print("Berichtigung:")
-#print(n_rho)
+print(n_rho)
 print("")
 n_rho=unp.log(n_rho)
 
 
-AA=np.arange(20,360,20)
-BB=nom(n_rho)[0:17]
-CC=std(n_rho)[0:17]
+#
+# Info: Hier wird entschieden, welche Werte zu jeweiligen Gruppe gehoert
+#
+#
+FU=17
+AA=np.arange(20,20*FU+20,20)
+BB=nom(n_rho)[0:FU]
+CC=std(n_rho)[0:FU]
 
-AAA=np.arange(360,(1+len(N_rho))*20,20)
-BBB=nom(n_rho)[17:41]
-CCC=std(n_rho)[17:41]
+AAA=np.arange(20*FU+20,(1+len(N_rho))*20,20)
+BBB=nom(n_rho)[FU:41]
+CCC=std(n_rho)[FU:41]
 
 #print(AA)
 #print(BB)
@@ -97,17 +97,12 @@ errors = np.sqrt(np.diag(covariance))
 print("RHODIUM-Langlebige Parameter:")
 a=ufloat(params[0],errors[0])
 print("a={}".format(a))
-print("Da={}".format(a-0.002666))###############################################################################
-print("da={}".format((a-0.002666)/0.002666))###############################################################################
-print(0.0045/0.002666)
 a_star=a
 b=ufloat(params[1],errors[1])
 print("b={}".format(b))
 b_star=b
 print("")
 print('T ={}'.format(np.log(2)/-a))
-print('DT ={}'.format((np.log(2)/-a)-260))#############################################################
-print('dT ={}'.format(((np.log(2)/-a)-260)/260))#############################################################
 print('N0 ={}'.format(unp.exp(b)/(1-unp.exp(a*250))))
 print("")
 X = np.linspace(0, 800)
@@ -118,20 +113,18 @@ errors = np.sqrt(np.diag(covariance))
 print("RHODIUM-Kurzlebige Parameter:")
 a=ufloat(params[0],errors[0])
 print("a={}".format(a))
-print("Da={}".format(a-0.01639))################################################################
-print("da={}".format((a-0.01639)/0.01639))################################################################
-b=ufloat(params[1],errors[1])
+a=ufloat(params[0],errors[0])+a_star
+print("a={}".format(a))
+b=ufloat(params[1],errors[1])+b_star
 print("b={}".format(b))
 print("")
 print('T ={}'.format(np.log(2)/-a))
-print('DT ={}'.format((np.log(2)/-a)-90))#########################################################
-print('DT ={}'.format(((np.log(2)/-a)-90)/90))#########################################################
 print('N0 ={}'.format(unp.exp(b)/(1-unp.exp(a*250))))
 print("")
 X = np.linspace(0, 500)
 plt.plot(X, f(X, *params), 'b-', label='1. Ausgleichsgerade')
-plt.errorbar(AA,BB,yerr=CC,fmt="bx",label="1.Rhodium")
-plt.errorbar(AAA,BBB,yerr=CCC,fmt="gx",label="2.Rhodium")
+plt.errorbar(AA,BB,yerr=CC,fmt="bx",label="1.Gruppe Rhodium")
+plt.errorbar(AAA,BBB,yerr=CCC,fmt="gx",label="2.Gruppe Rhodium")
 plt.ylim(1.4,6.5)
 plt.xlabel('Zeit $t$ in s')
 plt.grid()
@@ -140,19 +133,19 @@ plt.ylabel(r'Logarithmierte Zerfallrate $\Delta N$ im Zeitintervall $\Delta t$')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("../Bilder/rhodium.pdf")
-plt.close()
+plt.show()
 
-plt.errorbar(AA,BB,yerr=CC,fmt="kx",label="Rhodium")
-plt.errorbar(AAA,BBB,yerr=CCC,fmt="kx")
-plt.ylim(1.4,6.5)
-plt.xlabel('Zeit $t$ in s')
-plt.grid()
-plt.xticks(np.arange(20,(1+len(N_rho))*20,60),np.arange(20,(1+len(N_rho))*20,60))
-plt.ylabel(r'Logarithmierte Zerfallrate $\Delta N$ im Zeitintervall $\Delta t$')
-plt.legend(loc="best")
-plt.tight_layout()
-plt.savefig("../Bilder/rhodium_show.pdf")
-plt.close()
+#plt.errorbar(AA,BB,yerr=CC,fmt="kx",label="Rhodium")
+#plt.errorbar(AAA,BBB,yerr=CCC,fmt="kx")
+#plt.ylim(1.4,6.5)
+#plt.xlabel('Zeit $t$ in s')
+#plt.grid()
+#plt.xticks(np.arange(20,(1+len(N_rho))*20,60),np.arange(20,(1+len(N_rho))*20,60))
+#plt.ylabel(r'Logarithmierte Zerfallrate $\Delta N$ im Zeitintervall $\Delta t$')
+#plt.legend(loc="best")
+#plt.tight_layout()
+#plt.savefig("../Bilder/rhodium_show.pdf")
+#plt.show()
 
 #for i in range(0,40):
 #	print(20*i+20)
